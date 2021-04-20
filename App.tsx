31ae0ff55b0927feb7f13df68e0ac7af80e274b9
bellafsr <bellafsr@gmail.com>
2021-04-20 14:50:04 -0300

    fix: putting logic inside function

--- App.tsx
+++ App.tsx
@@ -1,46 +1,48 @@
 import { View, Text } from "react-native";
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Provider as StoreProvider } from 'react-redux';
-import { Provider as PaperProvider } from 'react-native-paper';
-import Constants from 'expo-constants';
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider as StoreProvider } from "react-redux";
+import { Provider as PaperProvider } from "react-native-paper";
+import Constants from "expo-constants";
 
-import useCachedResources from './hooks/useCachedResources';
-import HomeComponent from './screens/HomeComponent.component';
+import useCachedResources from "./hooks/useCachedResources";
+import HomeComponent from "./screens/HomeComponent.component";
 
-import store from './store';
+import store from "./store";
+
+function checkIfIsOnEmulator() {
+	if (!Constants.isDevice) {
+		return (
+			<View
+				style={{
+					height: "100%",
+					alignItems: "center",
+					justifyContent: "center",
+					paddingHorizontal: 30,
+				}}
+			>
+				<Text style={{ fontSize: 20 }}>
+					Oops, this will not work on Snack in an Android/Iphone
+					emulator. Try it on your device!
+				</Text>
+			</View>
+		);
+	}
+	return <HomeComponent />;
+}
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
+	const isLoadingComplete = useCachedResources();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-		<StoreProvider store={store}>
-			<SafeAreaProvider>
-				<PaperProvider>
-					{	
-						() => {
-							if (!Constants.isDevice) {
-								return (
-									<View style={{height: '100%', alignItems: 'center', justifyContent: 'center', paddingHorizontal: 30}}>
-										<Text>
-											Oops, this will not work on Snack in
-											an Android/Iphone emulator. Try it on your
-											device!
-										</Text>
-									</View>
-								);
-							}
-							return (
-								<HomeComponent />
-							)
-						}
-					}
-				</PaperProvider>
-			</SafeAreaProvider>
-		</StoreProvider>
-	);
-  }
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+		return (
+			<StoreProvider store={store}>
+				<SafeAreaProvider>
+					<PaperProvider>{checkIfIsOnEmulator()}</PaperProvider>
+				</SafeAreaProvider>
+			</StoreProvider>
+		);
+	}
 }
